768ae0790610417ce61019f10d23e761c11c59f0
Eric <erho20@gmail.com>
2022-05-09 20:37:12 -0700

    FInish footer section, fix some styling

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import { images } from "../../constants";
+import "./Footer.scss";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="app__footer app__flex">
+      <p className="p-text">Follow Us:</p>
+      <div className="app__footer-icons">
+        <div className="app__flex">
+          <images.Instagram  style={{fill:"#784cb3"}} />
+        </div>
+        <div className="app__flex">
+          <images.Facebook style={{fill:"#784cb3"}} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
